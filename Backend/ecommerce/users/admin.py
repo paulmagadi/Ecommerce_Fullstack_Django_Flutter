095bc5cc1103015ae9ba83411563faed3f641daf
SQCodes <paulsqmagadi@gmail.com>
2024-05-31 16:58:14 +0300

    modified:   Backend/ecommerce/users/admin.py

--- Backend/ecommerce/users/admin.py
+++ Backend/ecommerce/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser, Profile, ShippingAddress
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active", "last_login", "date_joined")
+    list_filter = ("email", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    filter_horizontal = ("groups", "user_permissions",)
+    readonly_fields = ("id", "last_login", "date_joined",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(Profile)
+# admin.site.register(ShippingAddress)
