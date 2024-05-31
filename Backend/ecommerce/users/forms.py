2c1075826b43e37621fb56bde10d7f218022398f
SQCodes <paulsqmagadi@gmail.com>
2024-05-31 20:15:57 +0300

    modified:   Backend/ecommerce/users/__pycache__/forms.cpython-311.pyc 	modified:   Backend/ecommerce/users/forms.py

--- Backend/ecommerce/users/forms.py
+++ Backend/ecommerce/users/forms.py
@@ -16,28 +16,28 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ("email",)
 
 
-# class CustomUserRegistrationForm(UserCreationForm):
-#     first_name = forms.CharField(max_length=30, required=True)
-#     last_name = forms.CharField(max_length=30, required=True)
-#     email = forms.EmailField(required=True)
+class CustomUserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
 
-#     class Meta:
-#         model = CustomUser
-#         fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
-
-
-# class UpdateUserForm(UserChangeForm):
-#     password = None
-
-#     class Meta:
-#         model = CustomUser
-#         fields = ['first_name', 'last_name', 'email']
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
 
 
-# class UpdateUserPassword(PasswordChangeForm):
-#     class Meta:
-#         model = CustomUser
-#         fields = ['new_password1', 'new_password2']
+class UpdateUserForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email']
+
+
+class UpdateUserPassword(PasswordChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['new_password1', 'new_password2']
 
 
 # class UpdateInfoForm(forms.ModelForm):
