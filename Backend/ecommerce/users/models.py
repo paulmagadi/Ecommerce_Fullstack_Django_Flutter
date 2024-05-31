1761f5a5838f1a1738b3cff281bc60caa7d74792
SQCodes <paulsqmagadi@gmail.com>
2024-05-31 16:53:22 +0300

    modified:   Backend/ecommerce/main/__pycache__/models.cpython-311.pyc 	modified:   Backend/ecommerce/users/models.py

--- Backend/ecommerce/users/models.py
+++ Backend/ecommerce/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from users.managers import CustomUserManager
+from django.db.models.signals import post_save
 
-# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='email', unique=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
