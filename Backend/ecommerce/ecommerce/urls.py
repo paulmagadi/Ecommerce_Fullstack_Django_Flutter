607df4009b20aadd5e35e15914737a0afa0e1bb8
SQCodes <paulsqmagadi@gmail.com>
2024-06-02 15:58:13 +0300

    modified:   Backend/ecommerce/ecommerce/urls.py

--- Backend/ecommerce/ecommerce/urls.py
+++ Backend/ecommerce/ecommerce/urls.py
@@ -22,4 +22,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('api/', include('api.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
