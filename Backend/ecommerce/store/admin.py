df4a122a3cc7be4c0f42c5e24b58488cbbdf2876
SQCodes <paulsqmagadi@gmail.com>
2024-06-02 18:57:24 +0300

    modified:   Backend/ecommerce/store/__pycache__/admin.cpython-311.pyc 	modified:   Backend/ecommerce/store/admin.py

--- Backend/ecommerce/store/admin.py
+++ Backend/ecommerce/store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product, Color, Size, ProductImage, WebBanner, MobileBanner
 from mptt.admin import MPTTModelAdmin
 
 
@@ -9,3 +9,9 @@ class CategoryAdmin(MPTTModelAdmin):
     search_fields = ('name',)
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(ProductImage)
+admin.site.register(WebBanner)
+admin.site.register(MobileBanner)
