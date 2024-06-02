375018d0b3f9a800e384f42bf5be99772747eba1
SQCodes <paulsqmagadi@gmail.com>
2024-06-02 22:50:20 +0300

    modified:   Backend/ecommerce/store/__pycache__/models.cpython-311.pyc 	modified:   Backend/ecommerce/store/models.py

--- Backend/ecommerce/store/models.py
+++ Backend/ecommerce/store/models.py
@@ -5,10 +5,6 @@ from mptt.models import MPTTModel, TreeForeignKey
 from django.core.exceptions import ValidationError
 from PIL import Image
 
-# def validate_image_size(image):
-#     max_size_kb = 1024  # Maximum size in KB
-#     if image.size > max_size_kb * 1024:
-#         raise ValidationError(f"Image size should not exceed {max_size_kb} KB")
 
 class Category(MPTTModel):
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
@@ -54,13 +50,14 @@ class Size(models.Model):
 
 class Product(models.Model):
     profile_image = models.ImageField(upload_to='uploads/products', null=True, blank=True, default='media/default/product.png')
-    name = models.CharField(max_length=255, verbose_name='product_name')
+    name = models.CharField(max_length=255, verbose_name='Product Name')
     price = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField(verbose_name='Product Description', blank=True, null=True)
     is_sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     discount = models.DecimalField(default=0, max_digits=9, decimal_places=2, null=True, blank=True)
     percentage_discount = models.DecimalField(default=0, max_digits=5, decimal_places=0, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     is_listed = models.BooleanField(default=True)
     is_featured = models.BooleanField(default=True)
     slug = models.SlugField(unique=True, blank=True)
@@ -82,6 +79,7 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         self.full_clean()
+        
         if self.stock_quantity == 0:
             self.is_listed = False
         else:
@@ -114,11 +112,15 @@ class Product(models.Model):
         except:
             url = ''
         return url
-    
+
     @property
     def is_new(self):
         now = timezone.now()
         return (now - self.created_at).days <= 30
+    
+    @property
+    def in_stock(self):
+        return self.stock_quantity > 0
 
 
 class ProductImage(models.Model):
