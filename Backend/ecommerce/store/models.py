a8bb68ca169149712fface2bb896648bef937f8a
SQCodes <paulsqmagadi@gmail.com>
2024-06-02 14:03:18 +0300

    modified:   Backend/ecommerce/store/models.py

--- Backend/ecommerce/store/models.py
+++ Backend/ecommerce/store/models.py
@@ -1,17 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
 from mptt.models import MPTTModel, TreeForeignKey
 
-
-
 class Category(MPTTModel):
-    name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    name = models.CharField(max_length=50, unique=True)
+    key_words = models.CharField(max_length=255)
+    descriptions = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='media/uploads/categories/')
+    slug = models.SlugField(unique=True, blank=True)
 
     class MPTTMeta:
         order_insertion_by = ['name']
-        
+
     class Meta:
-        verbose_plural = 'Categories'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
