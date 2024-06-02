3ac4ff63de9b6877f2cc464cc856feeba417db23
SQCodes <paulsqmagadi@gmail.com>
2024-06-02 15:01:29 +0300

    modified:   Backend/ecommerce/store/models.py

--- Backend/ecommerce/store/models.py
+++ Backend/ecommerce/store/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils.text import slugify
 from mptt.models import MPTTModel, TreeForeignKey
+from django.db.models.signals import post_save
+
+
+from django.utils import timezone      
+from django.core.exceptions import ValidationError
 
 class Category(MPTTModel):
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
@@ -42,9 +47,21 @@ class Images(models.Model):
     
     
 class Product(models.Model):
+    profile_image = models.ImageField(upload_to='media/uploads/products', null=True, blank=True, default='media/default/product.png')
     name = models.CharField(max_length=255, verbose_name='product_name')
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    profile_image = models.ImageField(upload_to='media/uploads/products')
+    is_sale =  models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=12, decimal_places=2)
+    discount = models.DecimalField(default=0, max_digits=9, decimal_places=2, null=True, blank=True)
+    percentage_discount = models.DecimalField(default=0, max_digits=5, decimal_places=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    is_listed = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True)
+    key_words = models.CharField(max_length=255)
+    stock_quantity = models.IntegerField(default=1)
+    brand = models.CharField(max_length=255, blank=True, null=True)
+    material = models.CharField(max_length=255, blank=True, null=True)
     category = TreeForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     color = models.ManyToManyField(Color, on_delete=models.CASCADE, related_name='products')
     size = models.ManyToManyField(Size, on_delete=models.CASCADE, related_name='products')
@@ -52,3 +69,40 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def clean(self):
+        super().clean()
+        if self.stock_quantity < 0:
+            raise ValidationError({'stock_quantity': 'Stock quantity cannot be less than 0'})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  
+            
+        if self.stock_quantity == 0:
+            self.in_stock = False
+        else:
+            self.in_stock = True
+
+        if self.is_sale and self.sale_price < self.price:
+            self.discount = round(self.price - self.sale_price, 2)
+            self.percentage_discount = round((self.discount / self.price) * 100)
+        else:
+            self.discount = 0
+            self.percentage_discount = 0
+
+        super().save(*args, **kwargs)
+
+   
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.profile_image.url
+        except:
+            url = ''
+        return url
