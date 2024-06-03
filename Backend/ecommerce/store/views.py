70f971dcd82914735a0476fcb65e8b4d327fb7ae
SQCodes <paulsqmagadi@gmail.com>
2024-06-03 16:24:38 +0300

    modified:   Backend/ecommerce/store/views.py

--- Backend/ecommerce/store/views.py
+++ Backend/ecommerce/store/views.py
@@ -62,7 +62,7 @@ def new(request):
 def featured(request):
     products = Product.objects.filter(is_featured=True)
     now = timezone.now()
-    new_product_ids = products.filter(created_at__gte=now - datetime.timedelta(hours=3)).values_list('id', flat=True)
+    new_product_ids = products.filter(created_at__gte=now - datetime.timedelta(days=30)).values_list('id', flat=True)
     context = {
         'products': products, 'new_products': new_product_ids,
     }
