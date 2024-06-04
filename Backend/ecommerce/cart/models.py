d94f77c1525562bfec0d4fe25a0f6d9904753aff
SQCodes <paulsqmagadi@gmail.com>
2024-06-04 20:20:49 +0300

    modified:   Backend/ecommerce/cart/__pycache__/models.cpython-311.pyc 	modified:   Backend/ecommerce/cart/models.py

--- Backend/ecommerce/cart/models.py
+++ Backend/ecommerce/cart/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from store.models import Product
+from users.models import CustomUser
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_address = models.TextField(max_length=15000)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    # color = models.CharField(max_length=50, null=True, blank=True)
+    # size = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"OrderItem {self.id} for Order {self.order.id}"
