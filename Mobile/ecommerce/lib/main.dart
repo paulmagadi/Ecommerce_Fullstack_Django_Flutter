d447d525abcd344087a4424c9603f933b166e1b8
SQCodes <paulsqmagadi@gmail.com>
2024-06-06 23:56:44 +0300

    modified:   Mobile/ecommerce/lib/main.dart

--- Mobile/ecommerce/lib/main.dart
+++ Mobile/ecommerce/lib/main.dart
@@ -7,25 +7,27 @@ import 'screens/static/contact.dart';
 import 'screens/static/help.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (ctx) => Cart(),
       child: MaterialApp(
-        title: 'E-commerce App',
+        title: 'Bellamore Apparels',
         theme: ThemeData(
           primarySwatch: Colors.orange,
         ),
-        home: HomePage(),
+        home: const HomePage(),
         routes: {
           '/about': (context) => AboutPage(),
           '/help': (context) => HelpPage(),
           '/contact': (context) => ContactUsPage(),
-        }, //Home view
+        }, 
       ),
     );
   }
