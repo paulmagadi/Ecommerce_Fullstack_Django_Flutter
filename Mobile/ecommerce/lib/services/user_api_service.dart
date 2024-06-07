c10424de43a71b19dbc28fef66a35abec3ca56c4
SQCodes <paulsqmagadi@gmail.com>
2024-06-07 20:16:32 +0300

    new file:   Mobile/ecommerce/lib/services/user_api_service.dart 	modified:   Mobile/ecommerce/macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   Mobile/ecommerce/pubspec.lock 	modified:   Mobile/ecommerce/pubspec.yaml

---
+++ Mobile/ecommerce/lib/services/user_api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiService {
+  final String baseUrl = 'http://10.0.0.1/api/auth/';
+
+  Future<http.Response> postRequest(String endpoint, Map<String, dynamic> data) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    return await http.post(url, body: json.encode(data), headers: {
+      'Content-Type': 'application/json',
+    });
+  }
+
+  Future<http.Response> getRequest(String endpoint) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    return await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json',
+    });
+  }
+
+  Future<void> storeToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+}
