367bb98e57fcb063f6c31153e80d386e405e96b0
SQCodes <paulsqmagadi@gmail.com>
2024-06-09 21:16:49 +0300

    modified:   Mobile/bellamore/lib/providers/auth_provider.dart

--- Mobile/bellamore/lib/providers/auth_provider.dart
+++ Mobile/bellamore/lib/providers/auth_provider.dart
@@ -26,10 +26,10 @@ class AuthProvider with ChangeNotifier {
       _token = data['token'];
       _isAuthenticated = true;
       final prefs = await SharedPreferences.getInstance();
-      prefs.setString('token', _token!);
+      await prefs.setString('token', _token!);
       notifyListeners();
     } else {
-      throw Exception('Failed to login');
+      throw Exception('Failed to login: ${response.body}');
     }
   }
 
@@ -44,7 +44,7 @@ class AuthProvider with ChangeNotifier {
     if (response.statusCode == 201) {
       await login(email, password);
     } else {
-      throw Exception('Failed to register');
+      throw Exception('Failed to register: ${response.body}');
     }
   }
 
@@ -53,7 +53,7 @@ class AuthProvider with ChangeNotifier {
     _isAuthenticated = false;
     _token = null;
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove('token');
+    await prefs.remove('token');
     notifyListeners();
   }
 
@@ -71,30 +71,29 @@ class AuthProvider with ChangeNotifier {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('token');
   }
-}
 
+  Future<http.Response> authenticatedRequest(String method, Uri url, {dynamic body}) async {
+    final token = await getToken();
+    if (token == null) {
+      throw Exception('No token found');
+    }
 
-Future<http.Response> _authenticatedRequest(String method, Uri url, {dynamic body}) async {
-  final token = await getToken();
-  if (token == null) {
-    throw Exception('No token found');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    switch (method.toUpperCase()) {
+      case 'POST':
+        return http.post(url, headers: headers, body: json.encode(body));
+      case 'GET':
+        return http.get(url, headers: headers);
+      case 'PUT':
+        return http.put(url, headers: headers, body: json.encode(body));
+      case 'DELETE':
+        return http.delete(url, headers: headers);
+      default:
+        throw Exception('Unsupported HTTP method');
+    }
   }
-  
-  final headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer $token',
-  };
-
-  switch (method.toUpperCase()) {
-    case 'POST':
-      return http.post(url, headers: headers, body: json.encode(body));
-    case 'GET':
-      return http.get(url, headers: headers);
-    // Add other HTTP methods as needed
-    default:
-      throw Exception('Unsupported HTTP method');
-  }
-}
-
-getToken() {
 }
