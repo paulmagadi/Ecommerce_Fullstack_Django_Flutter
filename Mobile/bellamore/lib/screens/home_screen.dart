ca25d84bc95033ec388160e0378b07301e5ac2e7
SQCodes <paulsqmagadi@gmail.com>
2024-06-09 16:52:34 +0300

    modified:   Mobile/bellamore/lib/screens/home_screen.dart

--- Mobile/bellamore/lib/screens/home_screen.dart
+++ Mobile/bellamore/lib/screens/home_screen.dart
@@ -5,6 +5,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(children: [
+          
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              'Categories',
+              style: TextStyle(fontSize: 34),
+            ),
+          ]),
+          CategoryView(),
+          SizedBox(height: 10,),
+          BannerCarousel(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              'Products',
+              style: TextStyle(fontSize: 34),
+            ),
+          ]),
+          ProductsView(),
+        ]),
+      ),
+    );
   }
 }
