63ac31e04897055a322ccf721b6079d29e5b9ca3
SQCodes <paulsqmagadi@gmail.com>
2024-06-10 15:43:29 +0300

    new file:   Mobile/bellamore/lib/screens/profile_info_form.dart

---
+++ Mobile/bellamore/lib/screens/profile_info_form.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:your_app/providers/auth_provider.dart';
+import 'package:your_app/providers/profile_provider.dart';
+
+class ProfileFormScreen extends StatefulWidget {
+  @override
+  _ProfileFormScreenState createState() => _ProfileFormScreenState();
+}
+
+class _ProfileFormScreenState extends State<ProfileFormScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _phoneController = TextEditingController();
+  final _address1Controller = TextEditingController();
+  final _address2Controller = TextEditingController();
+  final _cityController = TextEditingController();
+  final _stateController = TextEditingController();
+  final _zipcodeController = TextEditingController();
+  final _countryController = TextEditingController();
+
+  void _submitProfile() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await Provider.of<ProfileProvider>(context, listen: false).updateProfile(
+          phone: _phoneController.text,
+          address1: _address1Controller.text,
+          address2: _address2Controller.text,
+          city: _cityController.text,
+          state: _stateController.text,
+          zipcode: _zipcodeController.text,
+          country: _countryController.text,
+        );
+        Navigator.pushReplacementNamed(context, '/home');
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.toString())),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Complete Profile')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: 'Phone'),
+              ),
+              TextFormField(
+                controller: _address1Controller,
+                decoration: InputDecoration(labelText: 'Address 1'),
+              ),
+              TextFormField(
+                controller: _address2Controller,
+                decoration: InputDecoration(labelText: 'Address 2'),
+              ),
+              TextFormField(
+                controller: _cityController,
+                decoration: InputDecoration(labelText: 'City'),
+              ),
+              TextFormField(
+                controller: _stateController,
+                decoration: InputDecoration(labelText: 'State'),
+              ),
+              TextFormField(
+                controller: _zipcodeController,
+                decoration: InputDecoration(labelText: 'Zip Code'),
+              ),
+              TextFormField(
+                controller: _countryController,
+                decoration: InputDecoration(labelText: 'Country'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submitProfile,
+                child: Text('Submit'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/home');
+                },
+                child: Text('Skip'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
