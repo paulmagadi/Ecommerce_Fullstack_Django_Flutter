a986a962be439d16c76b23f1693d4ec4c857cbf9
SQCodes <paulsqmagadi@gmail.com>
2024-06-11 23:21:46 +0300

    modified:   Mobile/bellamore/lib/screens/home_screen.dart

--- Mobile/bellamore/lib/screens/home_screen.dart
+++ Mobile/bellamore/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bellamore/screens/home_view/product_view.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import '../../providers/product_provider.dart';
 import '../widgets/banner_carousel.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,29 +9,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              'Categories',
-              style: TextStyle(fontSize: 34),
-            ),
-          ]),
-          // CategoryView(),
-          SizedBox(
-            height: 10,
+    return ChangeNotifierProvider(
+      create: (_) => ProductProvider()..fetchProducts(), // Fetch products on widget creation
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Categories',
+                    style: TextStyle(fontSize: 34),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              BannerCarousel(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Products',
+                    style: TextStyle(fontSize: 34),
+                  ),
+                ],
+              ),
+              const ProductsView(), // ProductsView should be a child of ChangeNotifierProvider
+            ],
           ),
-          BannerCarousel(),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              'Products',
-              style: TextStyle(fontSize: 34),
-            ),
-          ]),
-          ProductsView(),
-        ]),
+        ),
       ),
     );
   }
