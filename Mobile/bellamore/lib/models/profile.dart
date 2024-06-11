d0d3298c710fa76a8ced1e02c64c6c45492920d9
SQCodes <paulsqmagadi@gmail.com>
2024-06-11 09:59:01 +0300

    modified:   Mobile/bellamore/lib/models/profile.dart

--- Mobile/bellamore/lib/models/profile.dart
+++ Mobile/bellamore/lib/models/profile.dart
@@ -1,49 +1,36 @@
-import 'package:flutter/material.dart';
+
 
 class Profile {
-    final Image image;
-    final String phone;
-    final String address1;
-    final String? address2;
-    final String city;
-    final String state;
-    final String zipcode;
-    final String country;
+  final String? image; // URL to the profile image
+  final String? phone;
+  final String? address1;
+  final String? address2;
+  final String? city;
+  final String? state;
+  final String? zipcode;
+  final String? country;
 
   Profile({
-    required  this.image,
-    required  this.phone,
-    required  this.address1,
+    this.image,
+    this.phone,
+    this.address1,
     this.address2,
-    required  this.city,
-    required  this.state,
-    required  this.zipcode,
-    required  this.country,
+    this.city,
+    this.state,
+    this.zipcode,
+    this.country,
   });
 
   factory Profile.fromJson(Map<String, dynamic> json) {
     return Profile(
-      image: json['image'],
-      phone : json['phone'],
-      address1 : json['address1'],
-      address2 : json['address2'],
-      city : json['city'],
-      state : json['state'],
-      zipcode : json['zipcode'],
-      country : json['country'],
+      image: json['image'], // Assumes the JSON has an 'image' field
+      phone: json['phone'],
+      address1: json['address1'],
+      address2: json['address2'],
+      city: json['city'],
+      state: json['state'],
+      zipcode: json['zipcode'],
+      country: json['country'],
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-
-      'phone' : phone,
-      'address1' : address1,
-      'address2' : address2,
-      'city' : city,
-      'state' : state,
-      'zipcode' : zipcode,
-      'country' : country,
-    };
   }
 }
