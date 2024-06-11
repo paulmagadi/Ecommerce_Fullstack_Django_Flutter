8ce3eb8e3a93b004d8e580afad3c9e6e0a366484
SQCodes <paulsqmagadi@gmail.com>
2024-06-12 01:35:54 +0300

    new file:   Mobile/bellamore/lib/screens/home_view/category_view.dart

---
+++ Mobile/bellamore/lib/screens/home_view/category_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../../models/product.dart';
+import '../../widgets/category_item.dart';
+
+class CategoryView extends StatelessWidget {
+  final List<Category> categories;
+
+  const CategoryView({Key? key, required this.categories}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,  // Number of columns in the grid
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: categories.length,
+      itemBuilder: (ctx, index) {
+        return CategoryItem(category: categories[index]);
+      },
+    );
+  }
+}
