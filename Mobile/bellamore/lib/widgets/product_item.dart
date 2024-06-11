4e48ad71f65d956c7249e3923a293f65c4545482
SQCodes <paulsqmagadi@gmail.com>
2024-06-11 18:55:42 +0300

    new file:   Mobile/bellamore/lib/widgets/product_item.dart

---
+++ Mobile/bellamore/lib/widgets/product_item.dart
@@ -0,0 +1,29 @@
+// widgets/product_item.dart
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black87,
+        title: Text(
+          product.name,
+          textAlign: TextAlign.center,
+        ),
+        subtitle: Text('\$${product.price.toString()}'),
+      ),
+    );
+  }
+}
