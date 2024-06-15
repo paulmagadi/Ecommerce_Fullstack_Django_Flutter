04f000f4cf99e2e63ccb4ed2408373c2fd3a01c5
SQCodes <paulsqmagadi@gmail.com>
2024-06-15 23:45:20 +0300

    modified:   Backend/ecommerce/db.sqlite3 	modified:   Mobile/bellamore/lib/models/shipping_address.dart 	new file:   Mobile/bellamore/lib/providers/shipping_provider.dart 	modified:   Mobile/bellamore/lib/screens/checkout_screen.dart 	modified:   Mobile/bellamore/lib/screens/payment_screen.dart

--- Mobile/bellamore/lib/screens/checkout_screen.dart
+++ Mobile/bellamore/lib/screens/checkout_screen.dart
@@ -1,12 +1,11 @@
+// screens/checkout_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/cart.dart';
 import '../providers/cart_provider.dart';
 import '../providers/shipping_address_provider.dart';
 import '../widgets/cart_item.dart';
 import 'payment_screen.dart';
-import 'shipping_address_form_screen.dart';
-// import '../models/cart.dart';
 
 class CheckoutScreen extends StatefulWidget {
   @override
@@ -21,8 +20,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   }
 
   Future<void> _loadShippingAddress() async {
-    final shippingProvider =
-        Provider.of<ShippingAddressProvider>(context, listen: false);
+    final shippingProvider = Provider.of<ShippingAddressProvider>(context, listen: false);
     await shippingProvider.fetchShippingAddress();
   }
 
@@ -31,133 +29,95 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
     final cartProvider = Provider.of<CartProvider>(context);
     final shippingProvider = Provider.of<ShippingAddressProvider>(context);
     final cartItems = cartProvider.cartItems.values.toList();
+    final totalAmount = cartProvider.totalAmount;
     final shippingAddress = shippingProvider.shippingAddress;
 
-    List<Map<String, dynamic>> convertCartItems(List<CartItem> cartItems) {
-      return cartItems.map((item) => item.toMap()).toList();
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Checkout'),
-      ),
+      appBar: AppBar(title: Text('Checkout')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (shippingAddress != null)
-              Card(
-                elevation: 3,
-                margin: const EdgeInsets.only(bottom: 20),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Icon(
-                        Icons.location_on,
-                        color: Colors.red,
-                        size: 30,
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              shippingAddress.fullName ?? '',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(shippingAddress.email ?? ''),
-                            Text(shippingAddress.phone ?? ''),
-                            Text(
-                              '${shippingAddress.address1 ?? ''}, '
-                              '${shippingAddress.address2 ?? ''}, '
-                              '${shippingAddress.city ?? ''}, '
-                              '${shippingAddress.state ?? ''}, '
-                              '${shippingAddress.zipcode ?? ''}, '
-                              '${shippingAddress.country ?? ''}',
-                              style: const TextStyle(fontSize: 12),
-                            ),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.edit, color: Colors.orange),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ShippingAddressForm(),
-                            ),
-                          ).then((_) => _loadShippingAddress());
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            const Text('Order Summary', style: TextStyle(fontSize: 18)),
+            if (shippingAddress != null) _buildShippingAddressCard(shippingAddress),
+            Text('Order Summary', style: TextStyle(fontSize: 18)),
             Expanded(
               child: ListView.builder(
                 itemCount: cartItems.length,
                 itemBuilder: (ctx, index) {
-                  final cartItem = cartItems[index];
-                  return CartItemWidget(cartItem: cartItem);
+                  return CartItemWidget(cartItem: cartItems[index]);
                 },
               ),
             ),
           ],
         ),
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(color: Colors.orange),
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      bottomNavigationBar: _buildBottomNavigationBar(totalAmount, cartProvider),
+    );
+  }
+
+  Widget _buildShippingAddressCard(ShippingAddress shippingAddress) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Total: \$${cartProvider.totalAmount.toStringAsFixed(2)}',
-                    style: const TextStyle(fontSize: 18)),
-                ElevatedButton(
-                  onPressed: () {
-                    final totalAmount = cartProvider.totalAmount;
-                    final cartItemsList =
-                        cartProvider.cartItems.values.toList();
-
-                    if (totalAmount > 0) {
-                      final convertedCartItems =
-                          convertCartItems(cartItemsList);
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PaymentScreen(
-                            totalAmount: totalAmount,
-                            cartItems: convertedCartItems,
-                          ),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                            content: Text(
-                                'Your cart is empty or the total amount is zero.')),
-                      );
-                    }
-                  },
-                  child: const Text('Continue to Payment'),
-                )
-              ],
+            Icon(Icons.location_on, color: Colors.red, size: 30),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text(
+                  //   shippingAddress.fullName,
+                  //   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  // ),
+                  // Text(shippingAddress.email),
+                  Text(
+                    '${shippingAddress.address1}, ${shippingAddress.city}, '
+                    '${shippingAddress.state}, ${shippingAddress.zipcode}, ${shippingAddress.country}',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(double totalAmount, CartProvider cartProvider) {
+    return Container(
+      color: Colors.orange,
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Total: \$${totalAmount.toStringAsFixed(2)}', style: TextStyle(fontSize: 18)),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PaymentScreen(
+                        totalAmount: totalAmount,
+                        cartItems: cartProvider.cartItemsMap,
+                      ),
+                    ),
+                  );
+                },
+                child: Text('Continue to Payment'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- Mobile/bellamore/lib/screens/payment_screen.dart
+++ Mobile/bellamore/lib/screens/payment_screen.dart
@@ -1,14 +1,14 @@
-// ignore_for_file: unused_field
+// screens/payment_screen.dart
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:http/http.dart' as http;
-import '../config.dart'; // Ensure this contains your base URL and other config
+import '../config.dart';
 
 class PaymentScreen extends StatefulWidget {
   final double totalAmount;
-  final List<Map<String, dynamic>> cartItems; // Add cart items
+  final List<Map<String, dynamic>> cartItems;
 
   PaymentScreen({required this.totalAmount, required this.cartItems});
 
@@ -30,45 +30,34 @@ class _PaymentScreenState extends State<PaymentScreen> {
     try {
       final client = http.Client();
 
-      // Step 1: Get CSRF token
-      final csrfResponse = await client.get(
-        Uri.parse('${Config.baseUrl}/api/get_csrf_token/'),
+      // Get CSRF token
+      final getResponse = await client.get(Uri.parse('${Config.baseUrl}/api/get_csrf_token/'));
+      final csrfToken = jsonDecode(getResponse.body)['csrfToken'];
+
+      if (csrfToken == null) {
+        throw Exception('Failed to retrieve CSRF token.');
+      }
+
+      // Post payment process
+      final postResponse = await client.post(
+        Uri.parse('${Config.baseUrl}/payment/process/'),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        body: jsonEncode({
+          'totalAmount': widget.totalAmount,
+          'cartItems': widget.cartItems,
+        }),
       );
 
-      if (csrfResponse.statusCode == 200) {
-        final responseBody = jsonDecode(csrfResponse.body);
-        final csrfToken = responseBody['csrfToken'];
-
-        if (csrfToken == null) {
-          print('Failed to retrieve CSRF token.');
-          return;
-        }
-
-        // Step 2: Process payment
-        final postResponse = await client.post(
-          Uri.parse('${Config.baseUrl}/payment/process/'),
-          headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'X-CSRFToken': csrfToken,
-          },
-          body: jsonEncode({
-            'totalAmount': widget.totalAmount,
-            // 'cartItems': widget.cartItems,
-          }),
-        );
-
-        if (postResponse.statusCode == 200) {
-          final responseData = jsonDecode(postResponse.body);
-          setState(() {
-            _checkoutUrl = responseData['approvalUrl'];
-          });
-        } else {
-          throw Exception(
-              'Failed to initialize payment: ${postResponse.statusCode}');
-        }
+      if (postResponse.statusCode == 200) {
+        final responseData = jsonDecode(postResponse.body);
+        setState(() {
+          _checkoutUrl = responseData['approvalUrl'];
+        });
       } else {
-        throw Exception(
-            'Failed to fetch CSRF token. Status code: ${csrfResponse.statusCode}');
+        throw Exception('Failed to initialize payment: ${postResponse.statusCode}');
       }
     } catch (e) {
       print('Error initializing payment: $e');
@@ -78,14 +67,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pay with PayPal'),
-      ),
+      appBar: AppBar(title: Text('Pay with PayPal')),
       body: _checkoutUrl != null
           ? InAppWebView(
               initialUrlRequest: URLRequest(url: Uri.parse(_checkoutUrl!)),
               initialOptions: InAppWebViewGroupOptions(
                 crossPlatform: InAppWebViewOptions(
+                  useOnLoadResource: true,
                   javaScriptEnabled: true,
                 ),
               ),
@@ -93,57 +81,22 @@ class _PaymentScreenState extends State<PaymentScreen> {
                 _webViewController = controller;
               },
               onLoadStop: (controller, url) async {
-                final returnUrl = '${Config.baseUrl}/payment/execute/';
-                final cancelUrl = '${Config.baseUrl}/payment/cancel/';
-
-                if (url.toString().startsWith(returnUrl)) {
-                  _handlePaymentSuccess(url.toString());
-                } else if (url.toString().startsWith(cancelUrl)) {
-                  Navigator.pop(context);
+                if (url?.host == 'success-url.com') {
+                  // Handle payment success
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Payment successful!')),
+                  );
+                } else if (url?.host == 'cancel-url.com') {
+                  // Handle payment cancellation
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Payment cancelled!')),
+                  );
                 }
               },
             )
-          : const Center(
-              child: CircularProgressIndicator(),
-            ),
+          : Center(child: CircularProgressIndicator()),
     );
-  }
-
-  
-
-  void _handlePaymentSuccess(String url) async {
-    final uri = Uri.parse(url);
-    final paymentId = uri.queryParameters['paymentId'] ?? '';
-    final payerId = uri.queryParameters['PayerID'] ?? '';
-
-    try {
-      final response = await http.post(
-        Uri.parse('${Config.baseUrl}/payment/execute/'),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode({
-          'paymentId': paymentId,
-          'payerId': payerId,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        final responseData = jsonDecode(response.body);
-        final orderId = responseData['order_id'];
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Payment Successful! Order ID: $orderId')),
-        );
-        Navigator.pop(context);
-      } else {
-        throw Exception('Failed to execute payment');
-      }
-    } catch (e) {
-      print('Error executing payment: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Payment Failed!')),
-      );
-    }
   }
 }
