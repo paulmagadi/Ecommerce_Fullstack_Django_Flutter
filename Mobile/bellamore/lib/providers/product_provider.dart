a97785704372519313da4c9760516ac7ba070fad
SQCodes <paulsqmagadi@gmail.com>
2024-06-18 23:58:49 +0300

    modified:   Mobile/bellamore/lib/providers/product_provider.dart

--- Mobile/bellamore/lib/providers/product_provider.dart
+++ Mobile/bellamore/lib/providers/product_provider.dart
@@ -44,19 +44,6 @@ class ProductProvider with ChangeNotifier {
     }
   }
 
-   Future<List<Product>> searchProducts(String query) async {
-    final url = Uri.parse('${Config.baseUrl}/api/products/?search=$query');
-    try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final List<dynamic> productData = json.decode(response.body);
-        return productData.map((data) => Product.fromJson(data)).toList();
-      } else {
-        throw Exception('Failed to load search results');
-      }
-    } catch (error) {
-      print('Error searching products: $error');
-      throw error;
-    }
-  }
+   
+  
 }
