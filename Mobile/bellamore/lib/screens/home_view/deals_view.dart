b098e6ec565c1f50198b4bd3a31009298f2f1c66
SQCodes <paulsqmagadi@gmail.com>
2024-06-22 23:01:51 +0300

    modified:   Mobile/bellamore/lib/screens/home_view/deals_view.dart

--- Mobile/bellamore/lib/screens/home_view/deals_view.dart
+++ Mobile/bellamore/lib/screens/home_view/deals_view.dart
@@ -4,14 +4,11 @@ import 'package:provider/provider.dart';
 import '../../providers/product_provider.dart';
 import '../../widgets/product_item.dart';
 
-
 class DealsView extends StatelessWidget {
   const DealsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final productProvider = Provider.of<ProductProvider>(context);
-
     return Consumer<ProductProvider>(
       builder: (context, productProvider, child) {
         if (productProvider.products.isEmpty) {
@@ -19,27 +16,28 @@ class DealsView extends StatelessWidget {
           if (!productProvider.isLoading) {
             productProvider.fetchProducts();
           }
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         } else {
-          final dealsProducts =
-              productProvider.products.where((product) => product.isSale).toList();
+          final dealsProducts = productProvider.products
+              .where((product) => product.isSale)
+              .toList();
 
           return Padding(
             padding: const EdgeInsets.all(4.0),
-            child: GridView.builder(
-              physics: NeverScrollableScrollPhysics(), // Disable GridView scrolling
-              shrinkWrap: true, // Ensure GridView takes only required space
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 2.3 / 3,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
+            child: SizedBox(
+              height: 200, // Specify a fixed height for the horizontal list
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: dealsProducts.length,
+                itemBuilder: (ctx, index) {
+                  final product = dealsProducts[index];
+                  return Container(
+                    width: 190, // Set a fixed width for each item
+                    padding: const EdgeInsets.all(4.0),
+                    child: ProductItem(product: product),
+                  );
+                },
               ),
-              itemCount: dealsProducts.length,
-              itemBuilder: (ctx, index) {
-                final product = dealsProducts[index];
-                return ProductItem(product: product);
-              },
             ),
           );
         }
