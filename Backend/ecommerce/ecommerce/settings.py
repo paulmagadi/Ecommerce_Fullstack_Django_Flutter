18e1d114c6f7e056d0191b0afadda310daf88117
SQCodes <paulsqmagadi@gmail.com>
2024-06-23 17:53:49 +0300

    modified:   Backend/ecommerce/db.sqlite3 	modified:   Backend/ecommerce/ecommerce/__pycache__/settings.cpython-311.pyc 	modified:   Backend/ecommerce/ecommerce/settings.py

--- Backend/ecommerce/ecommerce/settings.py
+++ Backend/ecommerce/ecommerce/settings.py
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'bellamore',
-		'USER': 'root',
-		'PASSWORD': 'Ra4kell@2020',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'bellamore',
+# 		'USER': 'root',
+# 		'PASSWORD': 'Ra4kell@2020',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
 
 
 
